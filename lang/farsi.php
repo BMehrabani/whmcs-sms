cb151a029f49b57a4e84371e704a899e2093e610
Babak Mehrabani <babak.mics@gmail.com>
2020-08-28 10:31:15 +0430

    Create farsi.php
    Translate to Persian (Farsi) language by @BMehrabani.

---
+++ lang/farsi.php
@@ -0,0 +1,52 @@
+<?php
+$_ADDONLANG['lang'] = "farsi";
+
+$_ADDONLANG['newversion'] = "یک آپدیت جدید برای پلاگین منتشر شده است. لطفا به <a href=\"https://github.com/AktuelSistem/WHMCS-SmsModule\">صفحه گیت هاب</a> مراجعه کنید و جدیدترین نسخه را دانلود کنید.<br><br>Please visit this page after file transfer finished.<br><br>";
+$_ADDONLANG['uptodate'] = "پلاگین شما به روز می باشد.";
+$_ADDONLANG['save'] = "دخیره";
+$_ADDONLANG['gsmnumberfield'] = "GSM/Mobile Number Field (Custom client field)";
+$_ADDONLANG['wantsmsfield'] = "Subscribe SMS Field (Custom client field)";
+$_ADDONLANG['user'] = "نام کاربری";
+$_ADDONLANG['pass'] = "کلمه عبور";
+$_ADDONLANG['apiid'] = "API ID";
+$_ADDONLANG['email'] = "ایمیل";
+$_ADDONLANG['countrycode'] = "پیش شماره کشور";
+$_ADDONLANG['senderid'] = "SenderID(عنوان)";
+$_ADDONLANG['dateformat'] = "فرمت تاریخ";
+$_ADDONLANG['sender'] = "ارسال کننده";
+$_ADDONLANG['settings'] = "تنظیمات";
+$_ADDONLANG['signature'] = "امضا";
+$_ADDONLANG['clientsmstemplates'] = "قالب کاربر";
+$_ADDONLANG['adminsmstemplates'] = "قالب مدیر";
+$_ADDONLANG['sendsms'] = "ارسال پیامک";
+$_ADDONLANG['messages'] = "ارسال پیام";
+$_ADDONLANG['update'] = "بروزرسانی";
+$_ADDONLANG['smssent'] = "<br><br><b>پیامک ارسال شد</b>";
+$_ADDONLANG['client'] = "کاربر";
+$_ADDONLANG['selectclient'] = "انتخاب کاربر";
+$_ADDONLANG['message'] = "پیام";
+$_ADDONLANG['debug'] = "چاپ گزارش";
+$_ADDONLANG['send'] = "ارسال";
+$_ADDONLANG['debugsonuc'] = "نتیجه";
+$_ADDONLANG['gsmnumber'] = "شماره تلفن همراه";
+$_ADDONLANG['datetime'] = "تاریخ و زمان";
+$_ADDONLANG['status'] = "وضعیت";
+$_ADDONLANG['delete'] = "حذف";
+$_ADDONLANG['parameter'] = "پارامتر:";
+$_ADDONLANG['active'] = "فعال";
+$_ADDONLANG['ekstra'] = "اضافه {x}";
+$_ADDONLANG['admingsm'] = "شماره تلفن همراه مدیر";
+$_ADDONLANG['admingsmornek'] = "می توانید شماره ها را با کاما از هم جدا کنید مثل: 09123456789,09353456789";
+$_ADDONLANG['lisans'] = "<a href=\"http://www.evmak.com\">توسعه داده شده توسط EvMak Tanzania و با تشکر از Aktüel</a> - <a href=\"https://github.com/emakundi/whmcs-sms\">Github Pageصفحه گیت هاب</a>. ترجمه فارسی توسط <a href=\"https://twitter.com/BMehrabani\">بابک مهربانی</a>.";
+$_ADDONLANG['credit'] = "اعتبار باقی مانده";
+
+$_ADDONLANG['error'] = "خطا";
+$_ADDONLANG['pending'] = "صف انتظار";
+$_ADDONLANG['sent'] = "ارسال شده";
+$_ADDONLANG['success'] = "موفق";
+
+$_ADDONLANG['authkey'] = "کلید تایید اعتبار";
+$_ADDONLANG['route'] = "مسیر تحویل پیامک (مثال: 4)";
+$_ADDONLANG['flash'] = "پیامک فلش (بله=1/خیر=0)";
+$_ADDONLANG['unicode'] = "یونیکد پیامک (بله=1/خیر=0)";
+$_ADDONLANG['ignoreNdnc'] = "شماره تلفن های بین المللی ندید گرفته شود؟ (بله=1/خیر=0)";
